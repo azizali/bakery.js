bb6e45dfe68a4d10c452c24dccac201913e710bf
okunishinishi <okunishinishi@gmail.com>
2012-10-28 09:41:55 +0900

    modify design

---
+++ doc/js/draw.api.js
@@ -0,0 +1,61 @@
+
+$.fn.extend({
+   asideMenu:function(section){
+       var menu = $(this),
+            ul = $('<ul/>').appendTo(menu).addClass('aside-menu');
+        $('fieldset', section).each(function(){
+            var fieldset = $(this),
+                id = fieldset.attr('id'),
+                legend = $('legend', fieldset).text();
+            var li = $('<li/>').appendTo(ul);
+            $('<a/>').attr('href', '#' + id)
+                .text(legend)
+                .appendTo(li);
+            $('<div/>')
+                .appendTo(li)
+                .asideSubMenu(fieldset);
+        });
+       var menuTop = menu.offset().top;
+       var win = $(window).scroll(function(){
+           var scrollTop = win.scrollTop();
+           if(scrollTop > menuTop){
+               menu.css({
+                   position:'fixed',
+                   top:10
+               });
+           } else {
+                menu.css({
+                    position:'absolute',
+                    top:menuTop
+                })
+           }
+       });
+       return menu;
+   },
+    asideSubMenu:function(fieldset){
+        var menu = $(this),
+            ul = $('<ul/>').appendTo(menu).addClass('aside-sub-menu');
+        $('dt', fieldset).each(function(){
+           var dt = $(this),
+               id = dt.attr('id'),
+               title = dt.text().replace(/^.*\./, '');
+            var li = $('<li/>').appendTo(ul);
+            if(!id){
+                id = 'dt-' + fieldset.prevAll('fieldset').size()
+                 + '-' + dt.prevAll('dt').size();
+                dt.attr('id', id);
+            }
+            $('<a/>')
+                .attr('href', '#' + id)
+                .text(title)
+                .appendTo(li);
+        });
+
+        return menu;
+    }
+});
+
+$(function(){
+    var apiSection = $('#section-api');
+    $('aside').asideMenu(apiSection);
+});
